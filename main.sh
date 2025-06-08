9c1141df52152668aec6c11f78a8e00469f2914c
Lasse <sver1993ige@gmail.com>
2025-06-09 00:21:30 +0200

    basic hyprland support

--- main.sh
+++ main.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 ZATHURA_CONFIG_DIR="$HOME/.config/zathura/"
+HYPRLAND_JSON_FILE="$ZATHURA_CONFIG_DIR/hypr.json"
 
 # set to 1 for the window manager you use
-DWM=1
-HYPRLAND=0
+DWM=0
+HYPRLAND=1
 
 move_on_stack () {
     # moves a process on the stack
@@ -85,6 +86,7 @@ pid_in_output () {
 }
 trap exit TERM
 
+
 pids=$(pgrep zathura)
 for pid in $pids; do
     # replace the -c option and potentially reapply quotes around -x option
@@ -98,19 +100,52 @@ for pid in $pids; do
             continue
         fi
         # loc includes the windows that should be moved in front of zathura again
+        # maybe to much magic but you'll figure it out if you want to ;)
         loc=$(echo "$wmctrl_output" | get_stack_location_dwm $pid)
         location=$(get_location $loc)
         echo $location
         active_window=$(xdotool getactivewindow)
+    elif [[ $HYPRLAND = 1 ]]; then
+        hyprctl clients -j > $HYPRLAND_JSON_FILE
+        active_window=$(hyprctl activewindow -j | jq -r ".address")
     fi
 
     kill $pid
     eval $newcmd &
+    echo $newcmd
 
     if [[ $DWM = 1 ]]; then
         sleep 0.2  # hope zathura created a window by now
         echo $loc
         put_window_on_master $loc # this should not be quoted
         xdotool windowactivate $active_window
+    elif [[ $HYPRLAND = 1 ]]; then
+        sleep 0.2 # hope zathura created a window by now
+        length=$(jq '. | length' $HYPRLAND_JSON_FILE)
+        for ((i=0; i<length; i++)); do
+            if [[ $(jq -r ".[$i].pid" $HYPRLAND_JSON_FILE) = $pid ]]; then
+                hyprctl dispatch movetoworkspace "$(jq -r ".[$i].workspace.id" $HYPRLAND_JSON_FILE)"
+                for (( j=0; j<4; j++ )); do
+                    xloc_wanted=$(jq -r ".[$i].at" $HYPRLAND_JSON_FILE | tr -d '[]\n' | cut -d',' -f1)
+                    xloc_current=$(hyprctl activewindow -j | jq -r ".at" | tr -d '[]\n' | cut -d',' -f1)
+                    yloc_wanted=$(jq -r ".[$i].at" $HYPRLAND_JSON_FILE | tr -d '[]\n' | cut -d',' -f2)
+                    yloc_current=$(hyprctl activewindow -j | jq -r ".at" | tr -d '[]\n' | cut -d',' -f2)
+                    if [[ $xloc_wanted -lt $xloc_current ]]; then
+                        hyprctl dispatch movewindow l
+                    elif [[ $xloc_wanted -gt $xloc_current ]]; then
+                        hyprctl dispatch movewindow r
+                    elif [[ $yloc_wanted -lt $yloc_current ]]; then
+                        hyprctl dispatch movewindow u
+                    elif [[ $yloc_wanted -gt $yloc_current ]]; then
+                        hyprctl dispatch movewindow d
+                    else
+                        break
+                    fi
+                done
+                hyprctl dispatch setprop activewindow at "$(jq -r ".[$i].at" $HYPRLAND_JSON_FILE | tr -d '[]')"
+                # hyprctl dispatch resizewindow "$()"
+            fi
+        done
+        hyprctl dispatch focuswindow address:$active_window
     fi
 done
